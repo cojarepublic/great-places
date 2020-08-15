5e89b8ddb525d3051d5f320a494b04765b1d5e64
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-15 17:49:33 +0100

    Storing the image on th device file system

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -14,6 +16,12 @@ class _ImageInputState extends State<ImageInput> {
   Future<void> _takePicture () async {
     final picker = ImagePicker();
     final imageFile = await picker.getImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
   }
 
   @override
