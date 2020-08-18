6ecb32da7df5ead8cb3bdee11ca4fe15a49016a3
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-18 06:39:41 +0100

    added rendering a dynamic map via Google

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,7 @@
-const GOOGLE_API_KEY = 'AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI';
+const GOOGLE_API_KEY = 'AIzaSyA3bSXaxzfHv9dK49gQ7H_6nH_a0XjgNec';
 
 class LocationHelper {
   static String generateLocationPreviewImage({double latitude, double longitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C&$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/screens/map_screen.dart';
 import 'package:location/location.dart';
 import 'package:great_places/helpers/location_helper.dart';
 
@@ -20,6 +21,21 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    // ...
   }
 
   @override
@@ -52,7 +68,7 @@ class _LocationInputState extends State<LocationInput> {
               textColor: Theme.of(context).primaryColor,
             ),
             FlatButton.icon(
-              onPressed: () {},
+              onPressed: _selectOnMap,
               icon: Icon(Icons.map),
               label: Text('Select on Map'),
               textColor: Theme.of(context).primaryColor,
